9bb8dc74da5f9f733681f60f2c59eb45e24a022d
lucas <lucas.gatica79@gmail.com>
2022-09-07 19:17:06 -0300

    inicio do js

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-console.log("teste")
+var palavras = ["arroz", "sapato", "cachorro","calopsita"]
+
+var quadro = document.querySelector("canvas");
+var pincel = quadro.getContext('2d');
+
+
+function telaaddpalavra(){
+
+}
+
+
+function addpalavra(palavra){
+    palavras.push(palavra);
+    console.log(palavras)
+
+}
+
+
+
+
+
+
+
